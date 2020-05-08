0f61ac3ab5f4b780a585f97a566d43d8cfbbe927
Daniel Do <daniel.minh.do@gmail.com>
2020-05-08 04:59:42 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -10,9 +10,9 @@ function callMe() {
 
 function crazy() {
   // fix the code in here:
+  var thisIsCrazy
   thisIsCrazy();
-
-  var thisIsCrazy = function (){
+  thisIsCrazy = function (){
     console.log("hey!!!")
   }
 }
